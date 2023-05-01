a20cad6f608176647de0e91df31159ffaa3d9d84
ChvrlixH <ehedzade.huseyn@gmail.com>
2023-05-01 07:31:14 +0400

    Admin(Info) management

--- FrontToBack/Areas/Admin/Controllers/InfoController.cs
+++ FrontToBack/Areas/Admin/Controllers/InfoController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Net;
+using System.Threading;
 
 namespace FrontToBack.Areas.Admin.Controllers
 {
@@ -14,6 +18,7 @@ namespace FrontToBack.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Info> faq = _context.FAQ.ToList();
+            ViewBag.Count = faq.Count;
 
             return View(faq);
         }
@@ -27,7 +32,95 @@ namespace FrontToBack.Areas.Admin.Controllers
 
         public IActionResult Create(Info info)
         {
+            if (!ModelState.IsValid)
+                return View();
+
+
+            if (_context.FAQ.Count() > 2)
+            {
+                Thread.Sleep(2000);
+                return BadRequest("Info is Full");
+            }
+
+
+
+                List<Info> faq = _context.FAQ.ToList();
+            foreach (var InfoItem in faq)
+            {
+                if (InfoItem.Title == info.Title)
+                {
+                    ModelState.AddModelError("Title", "This title is available");
+                    return View();
+                }
+            }
+
+
             _context.FAQ.Add(info);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Read(int id)
+        {
+            Info? info = _context.FAQ.AsNoTracking().FirstOrDefault(s => s.Id == id);
+            if (info is null)
+                return NotFound();
+
+            return View(info);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Info? info = _context.FAQ.FirstOrDefault(s => s.Id == id);
+            if (info is null)
+                return NotFound();
+
+            return View(info);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteInfo(int id)
+        {
+            Info? info = _context.FAQ.FirstOrDefault(s => s.Id == id);
+            if (info is null)
+                return NotFound();
+
+            _context.FAQ.Remove(info);
+            _context.SaveChanges();
+
+           return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Update(int id)
+        {
+            Info? info = _context.FAQ.FirstOrDefault(s => s.Id == id);
+            if (info is null)
+                return NotFound();
+
+            return View(info);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Info info, int id)
+        {
+            Info? dBinfo = _context.FAQ.AsNoTracking().FirstOrDefault(s => s.Id == id);
+            if (info is null)
+                return NotFound();
+
+            List<Info> faq = _context.FAQ.ToList();
+            foreach (var InfoItem in faq)
+            {
+                if (InfoItem.Title == info.Title)
+                {
+                    ModelState.AddModelError("Title", "This title is available");
+                    return View();
+                }
+            }
+
+            _context.FAQ.Update(info);
             _context.SaveChanges();
 
             return RedirectToAction(nameof(Index));
--- FrontToBack/Models/Info.cs
+++ FrontToBack/Models/Info.cs
@@ -5,8 +5,18 @@ namespace FrontToBack.Models
     public class Info
     {
         public int Id { get; set; }
+
+
+        [MaxLength(100), Required]
         public string Title { get; set; }
+
+
+        [MaxLength(100), Required]
         public string Description { get; set; }
-        public string Image { get; set; } 
+
+
+        [Required]
+        public string Image { get; set; }
+   
     }
 }
